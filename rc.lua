713bfe0e5e63f0d0b4d4c8ec09eebdebb8766c29
phoenix988 <karlfredin@gmail.com>
2023-05-12 23:58:45 +0300

    fixing syntax error

--- rc.lua
+++ rc.lua
@@ -38,7 +38,7 @@ do
 
         naughty.notify({ preset = naughty.config.presets.critical,
                          title = "Oops, an error happened!",
-                         text  tostring(err) })
+                         text = tostring(err) })
         in_error = false
     end)
 end
