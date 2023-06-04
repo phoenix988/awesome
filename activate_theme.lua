da52012927ad52a185561fc6ba88299c6d207da0
phoenix988 <karlfredin@gmail.com>
2023-06-04 23:34:10 +0300

    UPDATE: updating awesome structure

---
+++ activate_theme.lua
@@ -0,0 +1,16 @@
+
+-- Set your theme here
+-- available themes
+
+-- NOTE: This is only for colorscheme not diffrent widget layout
+-- All theme use the same widget layout just the colors are diffrent
+-- And maybe some icons
+
+--{{ dracula
+-- iceberg
+-- nord
+-- rose-pine
+-- tokyo-night}}
+local chosen_theme  = "iceberg"
+
+return chosen_theme
